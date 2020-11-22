00e18ee8b0856f0cff747cc7c5f75119221ea59c
Maks <maks125@yandex.ru>
2020-11-22 18:31:47 +0300

    change home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        КУ, привет здарова
+        <ul className="menu">
+          <li>
+            <a href="#">Главная</a>
+          </li>
+          <li>
+            <a href="#">Контакты</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
